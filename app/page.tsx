ccaeb4107f1c84d7721e5db8fad321d6b955f026
Lakshya429 <lakshyathakur429@gmail.com>
2024-09-09 13:47:41 +0530

    remove ensilit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-//@ts-expect-error
+//@ts-ignore
 import { Users, Radio, Headphones } from "lucide-react"
 import { Appbar } from "./components/Appbar"
 import useRedirect from "./hooks/useRedirect"
